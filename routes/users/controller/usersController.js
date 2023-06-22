2955dc16323e405da5c0994eb6b92b58abb4c9a6
Michael Spitz <michael.spitz@codeimmersives.com>
2023-06-22 14:49:05 -0400

    Final Backend Commit

--- routes/users/controller/usersController.js
+++ routes/users/controller/usersController.js
@@ -14,10 +14,7 @@ module.exports = {
       //   };
       // }
 
-      let checkedPassword = await comparePasswords(
-        req.body.password,
-        foundUser.password
-      );
+      let checkedPassword = await comparePasswords(req.body.password, foundUser.password);
       if (!checkedPassword) {
         throw {
           status: 401,
@@ -29,10 +26,18 @@ module.exports = {
         email: foundUser.email,
       };
 
+      let expiration = new Number
+      if(req.body.isRemember){
+        expiration = 60*60*24*7
+      }else{
+        expiration = 60*15
+      }
+
       let token = await jwt.sign(payload, process.env.SUPER_SECRET_KEY, {
-        expiresIn: 60 * 60,
+        expiresIn: expiration
       });
-      // console.log(foundUser)
+      
+      
       res.status(200).json({
         user: {
           email: foundUser.email,
